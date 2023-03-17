c9cd37a5ca7d74fcf391c48233bb3515257aa0e4
jaeseok-go <gojeasuk2@naver.com>
2023-03-17 21:41:36 +0900

    feat: MyBox 프로젝트 공통 Exception, 공통 Response 개발

---
+++ src/main/java/jaeseok/numble/mybox/common/response/MyBoxResponse.java
@@ -0,0 +1,30 @@
+package jaeseok.numble.mybox.common.response;
+
+import jaeseok.numble.mybox.common.response.exception.MyBoxException;
+
+public class MyBoxResponse {
+    private static final String SUCCESS_CODE = ErrorCode.SUCCESS.getCode();
+    private Boolean success;
+    private String errorCode;
+    private Object data;
+
+    public MyBoxResponse(Boolean success, String errorCode, Object data) {
+        this.success = success;
+        this.errorCode = errorCode;
+        this.data = data;
+    }
+
+    public MyBoxResponse(Object data) {
+        this.success = true;
+        this.errorCode = SUCCESS_CODE;
+        this.data = data;
+    }
+
+    public MyBoxResponse(MyBoxException exception) {
+        String errorCode = exception.getErrorCode();
+
+        this.success = SUCCESS_CODE.equals(errorCode);
+        this.errorCode = errorCode;
+        this.data = exception.getMessage();
+    }
+}
