040d4207931777a36ee5b9532feade3930a25bf5
jaeseok-go <gojeasuk2@naver.com>
2023-03-28 00:00:47 +0900

    test: 테스트 코드 리팩토링(MemberMapperTest 클래스 분리, 가독성을 위한 @Nested 활용)

--- src/test/java/jaeseok/numble/mybox/member/service/MemberServiceTest.java
+++ src/test/java/jaeseok/numble/mybox/member/service/MemberServiceTest.java
@@ -3,67 +3,57 @@ package jaeseok.numble.mybox.member.service;
 import jaeseok.numble.mybox.common.response.exception.MyBoxException;
 import jaeseok.numble.mybox.member.domain.Member;
 import jaeseok.numble.mybox.member.dto.MemberSignUpDto;
-import jaeseok.numble.mybox.member.dto.mapper.MemberMapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
 @Transactional
-@ActiveProfiles("dev")
+@DisplayName("MemberService Class")
 @SpringBootTest
 class MemberServiceTest {
-
     @Autowired
     MemberService memberService;
 
-    @Test
-    @DisplayName("MemberSignUpDto => Member 매핑 성공")
-    void mappingDtoToMemberSuccess() {
-        // given
-        MemberSignUpDto memberSignUpDto = new MemberSignUpDto("test_id", "1234", "테스트 닉네임");
+    private String id = "test_id";
+    private String password = "1234";
+    private String nickname = "테스트_닉네임";
 
-        // when
-        Member actual = MemberMapper.INSTANCE.toMember(memberSignUpDto);
-        Member expect = new Member("test_id", "1234","테스트 닉네임", LocalDateTime.now());
+    @Nested
+    @DisplayName("회원 가입")
+    class SignUp{
+        @Test
+        @DisplayName("성공")
+        void success() {
+            // given
+            MemberSignUpDto memberSignUpDto = new MemberSignUpDto(id, password, nickname);
 
-        // then
-        Assertions.assertEquals(expect, actual);
-    }
+            // when
+            String memberId = memberService.signUp(memberSignUpDto);
 
-    @Test
-    @DisplayName("회원가입 성공")
-    void signUpSuccess() {
-        // given
-        MemberSignUpDto memberSignUpDto = new MemberSignUpDto("test_id", "1234", "테스트 닉네임");
+            // then
+            Assertions.assertEquals(id, memberId);
+        }
 
-        // when
-        Member actual = memberService.signUp(memberSignUpDto);
-        Member expect = new Member("test_id", "1234","테스트 닉네임", LocalDateTime.now());
+        @Test
+        @DisplayName("실패 - 중복")
+        void fail_to_duplicate() {
+            // given
+            MemberSignUpDto memberSignUpDto = new MemberSignUpDto(id, password, nickname);
+            memberService.signUp(memberSignUpDto);
 
-        // then
-        Assertions.assertEquals(expect, actual);
-    }
+            // when
+            Executable signUp = () -> memberService.signUp(memberSignUpDto);
 
-    @Test
-    @DisplayName("회원가입 중복 실패")
-    void signUpDuplicateFail() {
-        // given
-        MemberSignUpDto memberSignUpDto = new MemberSignUpDto("test_id2", "1234", "테스트 닉네임");
-        memberService.signUp(memberSignUpDto);
-
-        // when
-        Executable signUp = () -> memberService.signUp(memberSignUpDto);
-
-        // then
-        MyBoxException e = Assertions.assertThrows(MyBoxException.class, signUp);
-        Assertions.assertEquals(e.getErrorCode(), "MBER0001");
+            // then
+            MyBoxException e = Assertions.assertThrows(MyBoxException.class, signUp);
+            Assertions.assertEquals(e.getErrorCode(), "MBER0001");
+        }
     }
 }
