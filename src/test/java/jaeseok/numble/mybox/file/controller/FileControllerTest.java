e1ae2d8c1b8fbdebc27324aa146f7c6002f5a75c
jaeseok-go <gojeasuk2@naver.com>
2023-07-30 14:20:03 +0900

    test: TestInitializer 사용하게 기존 코드 수정

--- src/test/java/jaeseok/numble/mybox/file/controller/FileControllerTest.java
+++ src/test/java/jaeseok/numble/mybox/file/controller/FileControllerTest.java
@@ -1,12 +1,10 @@
 package jaeseok.numble.mybox.file.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jaeseok.numble.mybox.common.response.MyBoxResponse;
 import jaeseok.numble.mybox.common.response.ResponseCode;
-import jaeseok.numble.mybox.member.dto.LoginParam;
 import jaeseok.numble.mybox.member.dto.LoginResponse;
-import jaeseok.numble.mybox.member.dto.SignUpParam;
 import jaeseok.numble.mybox.util.annotation.IntegrationTest;
+import jaeseok.numble.mybox.util.initializer.TestInitializer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -20,7 +18,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -37,6 +34,9 @@ class FileControllerTest {
     @Autowired
     protected ResourceLoader resourceLoader;
 
+    @Autowired
+    TestInitializer authorizedTestInitializer;
+
     @Nested
     @DisplayName("파일 업로드")
     class Upload {
@@ -48,27 +48,8 @@ class FileControllerTest {
 
         @BeforeEach
         void initMemberAndJwt() throws Exception {
-            // 회원가입
-            SignUpParam signUpParam = new SignUpParam(email, password);
-            mockMvc.perform(post("/api/v1/member")
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(objectMapper.writeValueAsString(signUpParam))
-                            .accept(MediaType.APPLICATION_JSON))
-                    .andDo(print());
-
-            // 로그인
-            LoginParam loginParam = new LoginParam(email, password);
-            ResultActions resultActions = mockMvc.perform(post("/api/v1/login")
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(objectMapper.writeValueAsString(loginParam))
-                            .accept(MediaType.APPLICATION_JSON))
-                    .andDo(print());
-
-            String content = resultActions.andReturn().getResponse().getContentAsString();
-            MyBoxResponse myBoxResponse = objectMapper.readValue(content, MyBoxResponse.class);
-
-            String json = objectMapper.writeValueAsString(myBoxResponse.getContent());
-            LoginResponse loginResponse = objectMapper.readValue(json, LoginResponse.class);
+            LoginResponse loginResponse = authorizedTestInitializer
+                    .memberInitialize(mockMvc, objectMapper, email, password);
 
             jwt = loginResponse.getJwt();
             rootFolderId = loginResponse.getRootFolderId();
--- src/test/java/jaeseok/numble/mybox/util/initializer/AuthorizedTestInitializer.java
+++ src/test/java/jaeseok/numble/mybox/util/initializer/AuthorizedTestInitializer.java
@@ -1,2 +1,49 @@
-package jaeseok.numble.mybox.util.initializer;public class AuthorizedTestInitializer {
+package jaeseok.numble.mybox.util.initializer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jaeseok.numble.mybox.common.response.MyBoxResponse;
+import jaeseok.numble.mybox.member.dto.LoginParam;
+import jaeseok.numble.mybox.member.dto.LoginResponse;
+import jaeseok.numble.mybox.member.dto.SignUpParam;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@Component
+public class AuthorizedTestInitializer implements TestInitializer{
+    @Override
+    public void signUp(MockMvc mockMvc, ObjectMapper objectMapper, String email, String password) throws Exception {
+        SignUpParam signUpParam = new SignUpParam(email, password);
+        mockMvc.perform(post("/api/v1/member")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(signUpParam))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print());
+    }
+
+    @Override
+    public LoginResponse login(MockMvc mockMvc, ObjectMapper objectMapper, String email, String password) throws Exception {
+        LoginParam loginParam = new LoginParam(email, password);
+        ResultActions resultActions = mockMvc.perform(post("/api/v1/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginParam))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print());
+
+        String content = resultActions.andReturn().getResponse().getContentAsString();
+        MyBoxResponse myBoxResponse = objectMapper.readValue(content, MyBoxResponse.class);
+
+        String json = objectMapper.writeValueAsString(myBoxResponse.getContent());
+        return objectMapper.readValue(json, LoginResponse.class);
+    }
+
+    @Override
+    public LoginResponse memberInitialize(MockMvc mockMvc, ObjectMapper objectMapper, String email, String password) throws Exception {
+        signUp(mockMvc, objectMapper, email, password);
+        return login(mockMvc, objectMapper, email, password);
+    }
 }
